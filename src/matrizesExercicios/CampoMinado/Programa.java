67f5747cf05fa87ebb8b067f8030566974c0017f
Matheus-Ferreira-S1lva <mf2110629@gmail.com>
2026-07-20 21:54:14 -0300

    Exercício básico de POO sobre comportamento de memória, arrays e listas

---
+++ src/matrizesExercicios/CampoMinado/Programa.java
@@ -0,0 +1,36 @@
+package matrizesExercicios.CampoMinado;
+
+import java.util.Scanner;
+
+public class Programa {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite o tamanho do Terreno: ");
+        int n = teclado.nextInt();
+        int m = teclado.nextInt();
+        int[][] mat = new int[n][m];
+
+        System.out.println("Digite o mapa (0 para seguro, 1 para mina):");
+        for (int i=0; i<mat.length; i++) {
+            for (int j=0; j< mat[i].length; j++) {
+                mat[i][j] = teclado.nextInt();
+            }
+        }
+
+        System.out.println();
+        System.out.println("Onde o robô vai pousar? (Linha e Coluna):");
+        System.out.print("Linha: ");
+        int linha = teclado.nextInt();
+        System.out.print("Coluna: ");
+        int coluna = teclado.nextInt();
+
+        Radar radar = new Radar(mat);
+
+        int totalBombas = radar.verificaBombas(linha, coluna);
+
+        System.out.println("ALERTA! O robô encontrou " + totalBombas + " minas ao redor da posição " + linha + "," + coluna);
+
+        teclado.close();
+    }
+}
---
+++ src/matrizesExercicios/CampoMinado/Radar.java
@@ -0,0 +1,31 @@
+package matrizesExercicios.CampoMinado;
+
+public class Radar {
+    private int[][] matriz;
+
+    public Radar(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public int verificaBombas(int linhas, int colunas) {
+        int bombasEncontradas = 0;
+
+        if (colunas > 0 && matriz[linhas][colunas - 1] == 1) {
+            bombasEncontradas++;
+        }
+
+        if (colunas < matriz[linhas].length - 1 && matriz[linhas][colunas + 1] == 1) {
+            bombasEncontradas++;
+        }
+
+        if (linhas > 0 && matriz[linhas - 1][colunas] == 1) {
+            bombasEncontradas++;
+        }
+
+        if (linhas < matriz.length - 1 && matriz[linhas + 1][colunas] == 1) {
+            bombasEncontradas++;
+        }
+
+        return bombasEncontradas;
+    }
+}
