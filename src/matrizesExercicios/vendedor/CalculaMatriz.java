5821641214c7a635efb280e5fa2c1bdb73d5a8a0
Matheus-Ferreira-S1lva <mf2110629@gmail.com>
2026-07-22 21:34:58 -0300

    Exercício básico de POO sobre comportamento de memória, arrays e listas

---
+++ src/matrizesExercicios/vendedor/CalculaMatriz.java
@@ -0,0 +1,18 @@
+package matrizesExercicios.vendedor;
+
+public class CalculaMatriz {
+
+    private int[][] matriz;
+
+    public CalculaMatriz(int[][] matriz) {
+        this.matriz = matriz;
+    }
+
+    public int somarColuna(int indexeColuna) {
+        int soma = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            soma += matriz[i][indexeColuna];
+        }
+        return soma;
+    }
+}
---
+++ src/matrizesExercicios/vendedor/Principal.java
@@ -0,0 +1,33 @@
+package matrizesExercicios.vendedor;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite as Linhas e Colunas: ");
+        int n = teclado.nextInt();
+        int m = teclado.nextInt();
+
+        int[][] mat = new int[n][m];
+
+        System.out.println("Digite os valores da matriz:");
+        for (int i=0; i < mat.length; i++) {
+            for (int j=0; j < mat[i].length; j++) {
+                mat[i][j] = teclado.nextInt();
+            }
+        }
+
+        System.out.println("Qual coluna você deseja somar?");
+        int coluna = teclado.nextInt();
+
+        CalculaMatriz procurar = new CalculaMatriz(mat);
+
+        int somaFinal = procurar.somarColuna(coluna);
+
+        System.out.print("A soma da coluna " + coluna + " é: " + somaFinal);
+
+        teclado.close();
+    }
+}
