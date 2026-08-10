dd46b174ea3c1e3e6d0ae5094426bfbc98e4a054
Matheus-Ferreira-S1lva <mf2110629@gmail.com>
2026-08-10 20:09:57 -0300

    Exercício básico de POO Enumerações e composição

---
+++ src/enumeracao_composicao/semafaro/CorSemaforo.java
@@ -0,0 +1,7 @@
+package enumeracao_composicao.semafaro;
+
+public enum CorSemaforo {
+    VERDE,
+    AMARELO,
+    VERMELHO;
+}
---
+++ src/enumeracao_composicao/semafaro/Programa.java
@@ -0,0 +1,10 @@
+package enumeracao_composicao.semafaro;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        CorSemaforo corAtual = CorSemaforo.AMARELO;
+
+        System.out.println("Cor atual do semaforo: " + corAtual);
+    }
+}
